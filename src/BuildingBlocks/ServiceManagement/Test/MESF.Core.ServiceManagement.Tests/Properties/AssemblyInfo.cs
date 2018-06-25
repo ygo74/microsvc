4764d4f2dfb65c077d4ce9f770a9111350507fe5
ygo74 <yannickgobert@yahoo.fr>
2018-06-25 22:09:18 +0200

    src

---
+++ src/BuildingBlocks/ServiceManagement/Test/MESF.Core.ServiceManagement.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MESF.Core.ServiceManagement.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MESF.Core.ServiceManagement.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3599a45e-2753-475b-aba0-fde4ceab6e49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/BuildingBlocks/ServiceManagement/src/MESF.Core.ServiceManagement/ServiceManagementWebHostBuilderExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MESF.Core.ServiceManagement
+{
+    public static class ServiceManagementWebHostBuilderExtension
+    {
+        public static IWebHostBuilder UseServiceManagement(this IWebHostBuilder builder)
+        {
+            return builder;
+        }
+    
+
+    }
+}
