5981a62b3f66109b17d242215de20b5055d19b12
ygo74 <yannickgobert@yahoo.fr>
2018-07-29 16:39:47 +0200

    redo vs structure

---
+++ src/BuildingBlocks/ServiceManagement/src/MESF.Core.ServiceManagement/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MESF.Core.ServiceManagement
+{
+    public class Class1
+    {
+        public static String GetValue()
+        {
+            return "test";
+        }
+    }
+}
